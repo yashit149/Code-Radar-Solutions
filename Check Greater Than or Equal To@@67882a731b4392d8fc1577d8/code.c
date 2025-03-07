8d120ae62ed70846628661dc8c72ad519ceca302
Yashit Mittal <yashit0077.becse24@chitkara.edu.in>
2025-03-07 15:12:35 +0530

    Solution to Check Greater Than or Equal To

--- Check Greater Than or Equal To@@67882a731b4392d8fc1577d8/code.c
+++ Check Greater Than or Equal To@@67882a731b4392d8fc1577d8/code.c
@@ -2,6 +2,10 @@
 int main(){
     int a,b;
     scanf("%d %d");
-    (a>=b)? printf("True"):printf("False");
-    return 0;
+    if(a>=b){
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
 }
